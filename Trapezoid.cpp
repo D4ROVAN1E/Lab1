b820ce1c438a45287c99a518029c47190fa66f21
D4ROVAN1E <garomiroved@mail.ru>
2025-03-02 20:56:25 +0700

    Added comments for trapezoid

--- Trapezoid.cpp
+++ Trapezoid.cpp
@@ -1,14 +1,14 @@
 #include <cmath>
 double perimetrTr(double firstSide, double secondSide, double upSide, double downSide)
 {
-	return firstSide + secondSide + upSide + downSide;
+	return firstSide + secondSide + upSide + downSide; //Сумма сторон
 }
-double squareTr(double firstSide, double secondSide, double upSide, double downSide)
+double squareTr(double firstSide, double secondSide, double upSide, double downSide) 
 {
-	double p = perimetrTr(firstSide, secondSide, upSide, downSide) / 2;
-	return ((upSide + downSide) / abs(downSide - upSide) * sqrt((p - downSide) * (p - upSide) * (p - downSide - firstSide) * (p - downSide - secondSide)));
+	double p = perimetrTr(firstSide, secondSide, upSide, downSide) / 2; //Полупериметр
+	return ((upSide + downSide) / abs(downSide - upSide) * sqrt((p - downSide) * (p - upSide) * (p - downSide - firstSide) * (p - downSide - secondSide))); //Площадь по формуле Герона 
 }
 double midLineTr(double firstSide, double secondSide, double upSide, double downSide)
 {
-	return ((upSide + downSide) / 2);
+	return ((upSide + downSide) / 2); //Полусумма оснований
 }
