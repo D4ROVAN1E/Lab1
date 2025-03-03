c7bf00e6ad125f2509c6e7e25b2642e7b8f1f6de
fanpopov <veselevvlad@gmail.com>
2025-03-03 04:05:31 +0000

    upd

--- triangle.cpp
+++ triangle.cpp
@@ -6,7 +6,7 @@ double area_heron(double firstSide, double secondSide, double downSide) {
     double polperimetr = (firstSide + secondSide + downSide) / 2;
     return sqrt(polperimetr * (polperimetr - firstSide) * (polperimetr - secondSide) * (polperimetr - downSide));//area_heron
 }
-double isosceles(double firstSide, double secondSide, double downSide) {
+double isosceles(double firstSide, double secondSide, double downSide){//proverka
     if ((firstSide == secondSide) || (secondSide == downSide) || (firstSide == downSide)) {
         return true;
     }
